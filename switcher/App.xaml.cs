877973bdd3a6d365a00420bb8877c56eb50dcbc2
Ilyas <uncognic@1707070.xyz>
2026-03-22 15:00:35 -0400

    add tray icon

--- switcher/App.xaml.cs
+++ switcher/App.xaml.cs
@@ -2,13 +2,15 @@
 
 namespace switcher
 {
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
         private Keyboard? _hook;
         private MainWindow? _window;
+        private NotifyIcon? _trayIcon;
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
             _window = new MainWindow();
 
             _hook = new Keyboard();
@@ -18,13 +20,30 @@ namespace switcher
                 else
                     _window.ShowSwitcher();
             };
-            _hook.OnAltReleased += () => _window.HideSwitcher();
+
             _hook.OnShiftTabPressed += () => _window.CyclePrev();
             _hook.OnAltReleased += () => _window.HideSwitcher();
             _hook.Install();
+
+            _trayIcon = new NotifyIcon
+            {
+                Icon = System.Drawing.SystemIcons.Application,
+                Visible = true,
+                Text = "Switcher"
+            };
+
+            var menu = new ContextMenuStrip();
+            menu.Items.Add("Exit", null, (_, _) =>
+            {
+                _trayIcon.Visible = false;
+                Shutdown();
+            });
+
+            _trayIcon.ContextMenuStrip = menu;
         }
         protected override void OnExit(ExitEventArgs e)
         {
+            _trayIcon?.Dispose();
             _hook?.Dispose();
             base.OnExit(e);
         }
